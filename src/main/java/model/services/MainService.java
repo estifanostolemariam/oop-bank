4cb6728646ea381b207f3e27f4b1a0d986144c6a
Estifanos Tolemariam <tolemariamestifanos@gmail.com>
2026-01-30 12:05:51 +0300

    Create MainService.java

---
+++ src/main/java/model/services/MainService.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.services;
+
+public class MainService {
+    private AuthService authService;
+    private ClientService clientService;
+    private AdminService adminService;
+    private UserRepository repo;
+
+    public MainService(UserRepository repo) {
+        this.repo = repo;
+        this.authService = new AuthService(repo);
+        this.clientService = new ClientService(repo);
+        this.adminService = new AdminService(repo);
+    }
+    
+    public AuthService getAuthService() { return authService; }
+    public ClientService getClientService() { return clientService; }
+    public AdminService getAdminService() { return adminService; }
+}
