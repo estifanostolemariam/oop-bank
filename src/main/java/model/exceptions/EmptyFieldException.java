23f43638bdc4a7600fcf2292ef5ff503032c7f92
Estifanos Tolemariam <tolemariamestifanos@gmail.com>
2026-01-30 11:53:28 +0300

    Create EmptyFieldException.java

---
+++ src/main/java/model/exceptions/EmptyFieldException.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.exceptions;
+
+public class EmptyFieldException extends Exception {
+    public EmptyFieldException(String msg) {
+        super(msg);
+    }
+}
