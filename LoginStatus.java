f8557a410f9c073dbe3d44bc99152e6e957e7977
Emran Awol <emranawol123@gmail.com>
2026-01-30 20:43:47 +0300

    Delete LoginStatus.java

--- LoginStatus.java
+++
@@ -1,7 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package model.services;
-
-public enum LoginStatus { SUCCESS, INVALID_CREDENTIALS, EMPTY_FIELDS }
