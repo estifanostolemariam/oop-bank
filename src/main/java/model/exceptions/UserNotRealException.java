119c2a381d4be56cd617c319f6add91143ff14af
Josias-Fasil <whynotjosias@gmail.com>
2026-01-31 12:51:40 +0300

    Add files via upload

---
+++ src/main/java/model/exceptions/UserNotRealException.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.exceptions;
+
+public class UserNotRealException extends Exception {
+    public UserNotRealException(String msg) {
+        super(msg);
+    }
+    
+}
